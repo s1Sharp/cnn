467869e34731827d49e22ec5dcc60bbb800bbc17
Maksim "s1Sharp" Tsarkov <maksim.carkov.201300@gmail.com>
2022-10-28 06:29:20 +0300

    Добавьте файлы проекта.

---
+++ cnn/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World\n");
+            return;
+        }
+    }
+}
