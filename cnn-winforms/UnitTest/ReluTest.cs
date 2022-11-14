37e40fbc3b7ea070519d6768be3c5640b5a1659e
Maksim "s1Sharp" Tsarkov <maksim.carkov.201300@gmail.com>
2022-11-14 03:53:46 +0300

    create test for Relu layer

---
+++ cnn-winforms/UnitTest/ReluTest.cs
@@ -0,0 +1,123 @@
+using CnnModule;
+using TorchSharp;
+using UnitTest.utils;
+
+namespace ReluTest
+{
+    [TestClass]
+    public class ReluConstruct
+    {
+        [TestMethod]
+        public void TestReluConstructDefault()
+        {
+            new Relu();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+            "A leaky_value cannot be apply for relu layer.")]
+        public void TestReluConstructWithNegativeLeaky()
+        {
+            new Relu(-0.01);
+        }
+
+        [TestMethod]
+        public void TestReluConstructWithPositiveLeaky()
+        {
+            new Relu(0.01);
+        }
+    }
+
+    [TestClass]
+    public class ReluForward
+    {
+        private const double dComparePrecision = 0.001;
+
+        [DataRow(
+            new long[] { 16, 10 }
+        )]
+        [DataRow(
+            new long[] { 1, 10 }
+        )]
+        [DataTestMethod]
+        public void TestReluForward(
+                long[] X_Shape
+            )
+        {
+            torch.random.manual_seed(0);
+
+            var x = torch.rand(X_Shape);
+
+            var expected_result = torch.nn.
+                functional.relu(x);
+
+            var r = new Relu();
+            var output = r.forward(x);
+
+            Assert.AreEqual(
+                expected_result,
+                output
+            );
+
+            Assert.AreEqual(
+                expected_result.shape,
+                output.shape
+            );
+
+            Assert.IsTrue(
+                new IsTensorAsFloat(output)
+            );
+
+            Assert.AreEqual(
+                0.0,
+                (double)(expected_result - output).mean(),
+                dComparePrecision
+            );
+        }
+
+
+        [DataRow(
+            new long[] { 16, 10 }
+        )]
+        [DataRow(
+            new long[] { 1, 10 }
+        )]
+        [DataTestMethod]
+        public void TestReluForwardWithPositiveLeaky(
+                long[] X_Shape
+            )
+        {
+            const double negative_slope = 0.1;
+            var x = torch
+                .distributions
+                .Uniform(-10.0, 10.0)
+                .sample(X_Shape);
+
+            var expected_result = torch.nn
+                .functional.leaky_relu(x, negative_slope);
+
+            var l = new Relu(negative_slope);
+            var output = l.forward(x);
+
+            Assert.AreEqual(
+                expected_result,
+                output
+            );
+
+            Assert.AreEqual(
+                expected_result.shape,
+                output.shape
+            );
+
+            Assert.IsTrue(
+                new IsTensorAsFloat(output)
+            );
+
+            Assert.AreEqual(
+                0.0,
+                (double)(expected_result - output).mean(),
+                dComparePrecision
+            );
+        }
+    }
+}
