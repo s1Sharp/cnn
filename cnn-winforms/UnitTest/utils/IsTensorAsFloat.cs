7268659a2321f0b3efd92dd61dd8f62d5983443d
Maksim "s1Sharp" Tsarkov <maksim.carkov.201300@gmail.com>
2022-11-14 03:53:24 +0300

    make utils create class to check Tensor datatype

---
+++ cnn-winforms/UnitTest/utils/IsTensorAsFloat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TorchSharp;
+
+namespace UnitTest.utils
+{
+    public class IsTensorAsFloat
+    {
+        private readonly bool _assert;
+        public IsTensorAsFloat(torch.Tensor input)
+        {
+            _assert = (
+                torch.float16 == input.dtype ||
+                torch.float32 == input.dtype ||
+                torch.float64 == input.dtype
+            );
+        }
+        public static implicit operator bool(IsTensorAsFloat v)
+        {
+            return v._assert;
+        }
+        public static bool operator true(IsTensorAsFloat v)
+        {
+            return v._assert;
+        }
+        public static bool operator false(IsTensorAsFloat v)
+        {
+            return v._assert;
+        }
+    }
+}
