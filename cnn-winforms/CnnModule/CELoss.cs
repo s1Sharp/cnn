30145f4d50b2f41d28d789d0cbd5e5d5dee2ab25
Maksim "s1Sharp" Tsarkov <maksim.carkov.201300@gmail.com>
2022-12-08 01:40:28 +0300

    cross entropy loss function

---
+++ cnn-winforms/CnnModule/CELoss.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static TorchSharp.torch;
+
+namespace CnnModule
+{
+    public class CELoss
+    {
+        public Tensor forward(Tensor input, Tensor target)
+        {
+            var result = (input.squeeze(-1) - target).pow(2.0).mean();
+            return result;
+        }
+
+        public double forward_double(Tensor input, Tensor target)
+        {
+            var result = (double)(input.squeeze(-1) - target).pow(2.0).mean();
+            return result;
+        }
+
+        public Tensor backward(Tensor output, Tensor target)
+        {
+            // grad of loss with respect to output of previous layer
+            return 2.0 * (output.squeeze() - target).unsqueeze(-1) / output.shape[0];
+        }
+    }
+}
